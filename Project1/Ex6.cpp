326ebcb5273a315bb5b65865e151b73663825981
João Martins <joaofmartins@ua.pt>
2021-11-15 18:32:02 +0000

    Resolução Ex6.

--- Project1/Ex6.cpp
+++ Project1/Ex6.cpp
@@ -11,15 +11,14 @@ int main(int argc, char* argv[]){
     bool loaded = audio.load(argv[1]);
     assert(loaded);
 
-    int channels = audio.getNumChannels(); // numero de canais no fhciero
-    int numSamples = audio.getNumSamplesPerChannel(); // numero de samples por canal
+    int channels = audio.getNumChannels();
+    int numSamples = audio.getNumSamplesPerChannel();
     map<double, int> hist;
 
-    // criaçao do histograma do ficheiro 
     for(int i = 0; i < channels; i++){
         for (int j = 0; j < numSamples; j++){
             double currentSample = audio.samples[i][j];
-            map<double, int>::  iterator it = hist.find(currentSample); //
+            map<double, int>::  iterator it = hist.find(currentSample);
             if(it != hist.end()){
                 it -> second++;
             }
@@ -28,12 +27,48 @@ int main(int argc, char* argv[]){
             }
         }
     }
-    ofstream ofs("audioHST.txt");
-    
-    // copia o histograma para um ficheiro 
+    ofstream ofs("audioHST_stereo.txt");
     for(auto it = hist.cbegin(); it != hist.cend(); ++it){ // precorre o iterador do mapa
         ofs << it->first << " - " << it->second << endl; // imprime first - second
     }
     ofs.close();
+
+    float entropySum = 0;
+    for (auto const& item : hist){
+        float prob = static_cast<float>(item.second)/numSamples;
+        entropySum += prob*log2(prob);
+    }
+    entropySum = -entropySum;
+    printf("Entropy stereo: %f\n", (entropySum));
+
+    //Mono Channel Histogram
+    map<double, int> histMono;
+    map<double, int>::  iterator it;
+
+    for (it = hist.begin(); it != hist.end(); it++)
+    {
+        map<double, int>::  iterator it_mono = histMono.find(abs(it->first));
+        if(it_mono != histMono.end()){
+            it_mono -> second += it->second;
+        }
+        else{
+            histMono.insert(make_pair(abs(it->first), it->second));
+        }
+    }
+
+    entropySum = 0;
+    for (auto const& item : histMono){
+        float prob = static_cast<float>(item.second)/numSamples;
+        entropySum += prob*log2(prob);
+    }
+    entropySum = -entropySum;
+    printf("Entropy Mono: %f\n", (entropySum));
+
+    ofstream ofs_mono("audioHST_mono.txt");
+    for(auto it = histMono.cbegin(); it != histMono.cend(); ++it){ // precorre o iterador do mapa
+        auto sampleValue = (it->second%2 == 0)? it->second / 2:it->second / 2 +1;
+        ofs_mono << it->first << " - " << sampleValue << endl; // imprime first - second
+    }
+    ofs_mono.close();
     return 0;
 }
