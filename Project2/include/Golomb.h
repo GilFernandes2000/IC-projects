885b1330903256e2425b7d128c6ace19e4e7e3ec
Gil <joaogilfernandes@ua.pt>
2021-11-26 19:58:57 +0000

    Golomb class made and tested

---
+++ Project2/include/Golomb.h
@@ -0,0 +1,52 @@
+#include <vector>
+#include <stdio.h>
+#include <assert.h>
+#include <string>
+#include <math.h>
+#include <bitset>
+#include <iostream>
+
+#ifndef PROJECT2_GOLOMB_H
+#define PROJECT2_GOLOMB_H
+
+using namespace std;
+
+class Golomb{
+    private:
+        signed int N;
+        int M;
+        int q;
+        int r;
+    public:
+        Golomb(double m, signed int n){
+            M = (int)m;
+            N = n;
+            q = floor(N/M);
+            r = N % M;
+        };
+        string enconding(){
+            // quocient code
+            string golomb;
+            for (int i = 0; i < q; i++){
+                golomb = golomb + '0';
+            }
+            golomb = golomb + '1';
+
+            string binary;
+            int b = log2(M);
+            binary = bitset<10>(r).to_string();
+            if(r < pow(2,b+1) - M){
+                for(int i = binary.length(); binary.length()-b < i; i--){
+                    golomb = golomb + binary[i];
+                }
+            }
+            else if(r >= pow(2,b+1) - M){
+                for(int i = binary.length(); binary.length()-b++ < i; i--){
+                    golomb = golomb + binary[i];
+                }
+            }
+            return golomb;
+        }
+};
+
+#endif //PROJECT2_GOLOMB_H
--- Project2/src/main.cpp
+++ Project2/src/main.cpp
@@ -1,4 +1,16 @@
 //
 // Created by joao- on 22/11/2021.
 //
+#include <iostream>
+#include "../include/BitStream.h"
+#include "../include/Golomb.h"
+
+
+int main(){
+    Golomb go(10, 11);
+    string encode = go.enconding();
+    cout << encode <<endl;
+    return EXIT_SUCCESS;
+}
+
 
